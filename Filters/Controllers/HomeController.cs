8c1eabd045b2d70438a1001bd2c2393989515c27
XanderUZZZER <xanderuzzzer@gmail.com>
2019-02-28 00:06:17 +0200

    Page 601

--- Filters/Controllers/HomeController.cs
+++ Filters/Controllers/HomeController.cs
@@ -3,12 +3,20 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Filters.Infrastructure;
 
 namespace Filters.Controllers
 {
+    //[HttpsOnly]
+    [Profile]
     public class HomeController : Controller
     {
-        public ViewResult Index() => 
-            View("Message", "This is the Index action on the Home controller");
+        //[RequireHttps]
+        public ViewResult Index() => View("Message",
+                    "This is the Index action on the Home controller");
+
+        //[RequireHttps]
+        public ViewResult SecondAction() => View("Message",
+                    "This is the SecondAction action on the Home controller");
     }
 }
